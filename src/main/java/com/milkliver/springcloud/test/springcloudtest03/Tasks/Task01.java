29fe7a8919c08613a2702d46c62c2ebf652fe13f
neil.yu <neil.yu@dynasafe.com.tw>
2022-05-03 14:39:20 +0800

    20220503 1439 test

--- src/main/java/com/milkliver/springcloud/test/springcloudtest03/Tasks/Task01.java
+++ src/main/java/com/milkliver/springcloud/test/springcloudtest03/Tasks/Task01.java
@@ -1,5 +1,7 @@
 package com.milkliver.springcloud.test.springcloudtest03.Tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -13,8 +15,7 @@ import com.milkliver.springcloud.test.springcloudtest03.utils.SendOmsAlert;
 @EnableTask
 public class Task01 {
 
-	@Value("${oms.server.url:#{null}}")
-	String omsServerUrlStr;
+	private static final Logger log = LoggerFactory.getLogger(Task01.class);
 
 	@Value("${oms.server.send-alert.connect-time-out:2000}")
 	int omsServerSendAlertConnectTimeOut;
@@ -34,15 +35,23 @@ public class Task01 {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			System.out.println("test");
 
-			if (omsServerSendAlertEnableHttps) {
-				sendOmsAlert.https(omsServerSendAlertUrl, "GET", omsServerSendAlertConnectTimeOut,
-						omsServerSendAlertReadTimeOut);
-			} else {
-				sendOmsAlert.http(omsServerSendAlertUrl, "GET", omsServerSendAlertConnectTimeOut,
-						omsServerSendAlertReadTimeOut);
+			log.info("CommandLineRunner ...");
+			try {
+				if (omsServerSendAlertEnableHttps) {
+					sendOmsAlert.https(omsServerSendAlertUrl, "GET", omsServerSendAlertConnectTimeOut,
+							omsServerSendAlertReadTimeOut);
+				} else {
+					sendOmsAlert.http(omsServerSendAlertUrl, "GET", omsServerSendAlertConnectTimeOut,
+							omsServerSendAlertReadTimeOut);
+				}
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				for (StackTraceElement elem : e.getStackTrace()) {
+					log.error(elem.toString());
+				}
 			}
+			log.info("CommandLineRunner finish");
 
 		};
 	}
