18f3bf56d1db00484d6fef6518b8c49f147bd905
neil.yu <neil.yu@dynasafe.com.tw>
2022-05-03 13:48:31 +0800

    20220503 1348 test

--- src/main/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01Application.java
+++ src/main/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springcloudtest03Application {
+public class Springcloudtest01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springcloudtest03Application.class, args);
+		SpringApplication.run(Springcloudtest01Application.class, args);
 	}
 
 }
--- src/test/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01ApplicationTests.java
+++ src/test/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springcloudtest03ApplicationTests {
+class Springcloudtest01ApplicationTests {
 
 	@Test
 	void contextLoads() {
