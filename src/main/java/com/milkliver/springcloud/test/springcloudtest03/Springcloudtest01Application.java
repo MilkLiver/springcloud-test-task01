a14a6296025c95868931ff0e7c85c686dc2fde09
neil.yu <neil.yu@dynasafe.com.tw>
2022-05-09 16:39:22 +0800

    20220509 1639 test

--- src/main/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01Application.java
+++ src/main/java/com/milkliver/springcloud/test/springcloudtest03/Springcloudtest01Application.java
@@ -5,16 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springcloudtest01Application implements ExitCodeGenerator {
+public class Springcloudtest01Application {
+//	public class Springcloudtest01Application implements ExitCodeGenerator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springcloudtest01Application.class, args);
 	}
 
-	@Override
-	public int getExitCode() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+//	@Override
+//	public int getExitCode() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
 
 }
