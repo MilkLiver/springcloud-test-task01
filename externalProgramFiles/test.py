f5eb687e789f1997894fbe4dfb59627399421ba3
neil.yu <neil.yu@dynasafe.com.tw>
2022-05-10 11:41:26 +0800

    20220510 1141 test

---
+++ externalProgramFiles/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 11:08:35 2022
+
+@author: user
+"""
+
+def main():
+    print("linux ZAWARUDO!!")
+
+if __name__=="__main__":
+    main()
--- src/main/java/com/milkliver/springcloud/test/springcloudtest03/Tasks/Task01.java
+++ src/main/java/com/milkliver/springcloud/test/springcloudtest03/Tasks/Task01.java
@@ -1,7 +1,9 @@
 package com.milkliver.springcloud.test.springcloudtest03.Tasks;
 
+import java.io.BufferedInputStream;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Locale;
 
@@ -22,6 +24,9 @@ public class Task01 {
 
 	private static final Logger log = LoggerFactory.getLogger(Task01.class);
 
+	final Base64.Decoder decoder = Base64.getDecoder();
+	final Base64.Encoder encoder = Base64.getEncoder();
+
 	@Value("${oms.server.send-alert.connect-time-out:2000}")
 	int omsServerSendAlertConnectTimeOut;
 
@@ -34,6 +39,9 @@ public class Task01 {
 	@Value("${oms.server.send-alert.enable-https:false}")
 	boolean omsServerSendAlertEnableHttps;
 
+	@Value("${system.command}")
+	String systemCommandBase64;
+
 	@Autowired
 	SendOmsAlert sendOmsAlert;
 
@@ -43,6 +51,28 @@ public class Task01 {
 
 			log.info("CommandLineRunner ...");
 			try {
+
+				log.info("CommandBase64: " + systemCommandBase64);
+				String systemCommand = new String(decoder.decode(systemCommandBase64));
+				log.info("Command: " + systemCommand);
+
+				Process process = Runtime.getRuntime().exec(systemCommand);
+
+				BufferedInputStream bufferedInputStream = new BufferedInputStream(process.getInputStream());
+
+				StringBuilder execCmdRes = new StringBuilder();
+
+				byte[] buffer = new byte[10240];
+				int bytesRead = 0;
+				while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+//					String chunk = new String(buffer, 0, bytesRead);
+					execCmdRes.append(new String(buffer, 0, bytesRead));
+				}
+
+				log.info(execCmdRes.toString());
+
+				process.destroy();
+
 				Date now = new Date();
 
 				URL sendAlertUrl = new URL(omsServerSendAlertUrl);
